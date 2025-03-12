23bec495cd979f156b3f54789a9fc6fcc9a9fa5e
aabccd021 <aabccd021@gmail.com>
2025-03-12 14:12:33 +0700

    Update default.nix to use lib.mapAttrs' for generating scripts mappings.

--- default.nix
+++ default.nix
@@ -1,12 +1,22 @@
 { pkgs }:
 let
+  lib = pkgs.lib;
+
   mkScript = name: runtimeInputs: pkgs.writeShellApplication {
     name = name;
     runtimeInputs = runtimeInputs;
     text = builtins.readFile "${./scripts}/${name}.sh";
   };
+
+  scripts = {
+    kill-fzf = [ pkgs.fzf pkgs.ps ];
+    get-repo = [ pkgs.gh pkgs.ghq pkgs.git ];
+  };
 in
-{
-  kill-fzf = mkScript "kill-fzf" [ pkgs.fzf pkgs.ps ];
-  get-repo = mkScript "get-repo" [ pkgs.gh pkgs.ghq pkgs.git ];
-}
+lib.mapAttrs'
+  (name: runtimeInputs: {
+    name = name;
+    value = mkScript name runtimeInputs;
+  })
+  scripts
+
