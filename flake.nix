13261ed9b2cc4324a6f57b5cf97c37376193ab9f
aabccd021 <aabccd021@gmail.com>
2025-05-21 21:00:55 +0700

    Revert "chore(flake): update devShells buildInputs configuration"
    This reverts commit 362439848112a6ee04977dcff74ab465424a9dbb.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
-        programs.nixfmt.strict = true;
         programs.prettier.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
@@ -33,7 +32,9 @@
 
       scripts = import ./default.nix { pkgs = pkgs; };
 
-      devShells.default = pkgs.mkShellNoCC { buildInputs = [ pkgs.nixd ]; };
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.nixd ];
+      };
 
       packages =
         scripts
