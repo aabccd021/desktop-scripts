072bb500d027cd926b7c31d97631777139899810
aabccd021 <aabccd021@gmail.com>
2025-05-30 05:31:56 +0700

    Refactored authentication logic and added error message if not authenticated.

--- scripts/get-my-repo.sh
+++ scripts/get-my-repo.sh
@@ -1,3 +1,10 @@
-gh repo list --limit 100 --json name --jq '.[].name' |
-  fzf |
-  xargs ghq get "$@"
+gh auth status >/dev/null 2>&1
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "You are not authenticated with GitHub. Please run 'gh auth login'."
+  exit "$exit_code"
+fi
+
+selected=$(gh repo list --limit 100 --json nameWithOwner --jq '.[].nameWithOwner' | fzf)
+ghq get "https://$(gh auth token)@github.com/$selected"
