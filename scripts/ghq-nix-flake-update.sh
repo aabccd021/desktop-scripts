a48422cec20e5ab95c300e7b064f399ce885dbd9
aabccd021 <aabccd021@gmail.com>
2025-06-16 08:10:44 +0700

    Update: improve flake update script for better directory handling.

--- scripts/ghq-nix-flake-update.sh
+++ scripts/ghq-nix-flake-update.sh
@@ -6,6 +6,8 @@ root_dir="$(ghq root)/github.com/$username"
 
 visited=""
 
+update_dirs=""
+
 update_flake() {
   node="$1"
 
@@ -15,32 +17,10 @@ update_flake() {
     owner=$(echo "$metadata" | jq --raw-output ".locks.nodes.\"$input\".original.owner")
     if [ "$owner" = "$username" ]; then
       repo=$(echo "$metadata" | jq --raw-output ".locks.nodes.\"$input\".original.repo")
+      update_dirs="$root_dir/$repo $update_dirs"
       update_flake "$input"
     fi
   done
-
-  if [ "$node" = "root" ]; then
-    update_dir="$dir"
-  else
-    update_dir="$root_dir/$repo"
-  fi
-
-  if [ ! -d "$update_dir" ]; then
-    ghq get "$username/$repo"
-  fi
-
-  for visited_dir in $visited; do
-    if [ "$visited_dir" = "$update_dir" ]; then
-      return
-    fi
-  done
-
-  cd "$update_dir" || exit 1
-  echo "Updating flake in $update_dir"
-  nix flake update --commit-lock-file
-  nix-checkpoint
-
-  visited="$visited $update_dir"
 }
 
 for dir in "$root_dir"/*/; do
@@ -49,6 +29,32 @@ for dir in "$root_dir"/*/; do
     continue
   fi
 
+  update_dirs="$dir $update_dirs"
   metadata=$(nix flake metadata "$dir" --json)
   update_flake "root"
+done
+
+for dir in $update_dirs; do
+  should_skip=false
+  for visited_dir in $visited; do
+    if [ "$visited_dir" = "$dir" ]; then
+      should_skip=true
+      break
+    fi
+  done
+
+  if [ "$should_skip" = true ]; then
+    echo "Skipping already visited directory: $dir"
+    continue
+  fi
+
+  if [ ! -d "$dir" ]; then
+    ghq get "$dir"
+  fi
+
+  cd "$dir" || exit 1
+  echo "Updating flake in $dir"
+  nix flake update --commit-lock-file
+  nix-checkpoint
+  visited="$visited $dir"
 done
