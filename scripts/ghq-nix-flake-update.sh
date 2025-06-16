99ddb9ea3c5af5e39e5a1f92e920551ade248b05
aabccd021 <aabccd021@gmail.com>
2025-06-16 08:18:39 +0700

    Update ghq-nix-flake-update.sh: Refactor update_flake function and directory handling

--- scripts/ghq-nix-flake-update.sh
+++ scripts/ghq-nix-flake-update.sh
@@ -6,55 +6,56 @@ root_dir="$(ghq root)/github.com/$username"
 
 visited=""
 
-update_dirs=""
-
 update_flake() {
   node="$1"
+  echo "Processing node: $node"
 
   inputs=$(echo "$metadata" | jq --raw-output ".locks.nodes.\"$node\".inputs | to_entries | map(.value) | .[]")
+  echo "Inputs for $node: $inputs"
 
   for input in $inputs; do
     owner=$(echo "$metadata" | jq --raw-output ".locks.nodes.\"$input\".original.owner")
+    echo "Processing input: $input, owner: $owner"
     if [ "$owner" = "$username" ]; then
       repo=$(echo "$metadata" | jq --raw-output ".locks.nodes.\"$input\".original.repo")
-      update_dirs="$root_dir/$repo $update_dirs"
       update_flake "$input"
     fi
   done
+
+  if [ "$node" = "root" ]; then
+    update_dir="$dir"
+  else
+    update_dir="$root_dir/$repo"
+  fi
+
+  if [ ! -d "$update_dir" ]; then
+    ghq get "$username/$repo"
+  fi
+
+  for visited_dir in $visited; do
+    if [ "$visited_dir" = "$update_dir" ]; then
+      echo "Already visited $update_dir, skipping"
+      return
+    fi
+  done
+
+  cd "$update_dir" || exit 1
+  echo "Updating flake in $update_dir"
+  nix flake update --commit-lock-file
+  nix-checkpoint
+
+  visited="$visited $update_dir"
 }
 
 for dir in "$root_dir"/*/; do
 
   if [ ! -f "$dir/flake.nix" ]; then
+    echo "$dir: Skipping non-flake"
     continue
   fi
 
-  update_dirs="$dir $update_dirs"
+  echo "Found flake.nix in $dir"
+
   metadata=$(nix flake metadata "$dir" --json)
   update_flake "root"
-done
-
-for dir in $update_dirs; do
-  should_skip=false
-  for visited_dir in $visited; do
-    if [ "$visited_dir" = "$dir" ]; then
-      should_skip=true
-      break
-    fi
-  done
-
-  if [ "$should_skip" = true ]; then
-    echo "Skipping already visited directory: $dir"
-    continue
-  fi
-
-  if [ ! -d "$dir" ]; then
-    ghq get "$dir"
-  fi
-
-  cd "$dir" || exit 1
-  echo "Updating flake in $dir"
-  nix flake update --commit-lock-file
-  nix-checkpoint
-  visited="$visited $dir"
 done
