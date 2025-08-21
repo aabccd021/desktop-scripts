09e0958af1ae287127f1008a7b57b1feb48a0cae
aabccd021 <aabccd021@gmail.com>
2025-08-21 12:52:20 +0700

    Add workflow file selection logic to run script.

--- scripts/run-gh-workflow.sh
+++ scripts/run-gh-workflow.sh
@@ -1,3 +1,5 @@
+workflow_file_input="${1:-}"
+
 repo_root=$(git rev-parse --show-toplevel || true)
 trap 'cd $(pwd)' EXIT
 cd "$repo_root" || exit 1
@@ -10,6 +12,8 @@ fi
 
 if [ "$(echo "$workflow_files" | wc -l)" -eq 1 ]; then
   selected_workflow_file=$(printf "%s" "$workflow_files")
+elif [ -n "$workflow_file_input" ]; then
+  selected_workflow_file="./.github/workflows/$workflow_file_input"
 else
   selected_workflow_file=$(echo "$workflow_files" | fzf)
 fi
