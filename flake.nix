4dc3d0b8206f66213c499eba668dbb23873c10e6
aabccd021 <aabccd021@gmail.com>
2025-10-02 08:05:41 +0700

    checkpoint

--- flake.nix
+++ flake.nix
@@ -7,19 +7,6 @@
   outputs =
     { self, ... }@inputs:
     let
-      lib = inputs.nixpkgs.lib;
-
-      collectInputs =
-        is:
-        pkgs.linkFarm "inputs" (
-          builtins.mapAttrs (
-            name: i:
-            pkgs.linkFarm name {
-              self = i.outPath;
-              deps = collectInputs (lib.attrByPath [ "inputs" ] { } i);
-            }
-          ) is
-        );
 
       overlay = (_: prev: import ./default.nix { pkgs = prev; });
 
@@ -55,7 +42,6 @@
         // {
           formatting = treefmtEval.config.build.check self;
           formatter = formatter;
-          allInputs = collectInputs inputs;
         };
 
     in
