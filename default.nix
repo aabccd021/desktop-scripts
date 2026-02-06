ad5e3e1db402ba560bbbe4dc5a4812d3054236b9
aabccd021 <aabccd021@gmail.com>
2026-02-06 10:19:43 +0700

    Disable inheritPath and add missing runtime dependencies
    Set inheritPath = false for writeShellApplication to ensure scripts
    only have access to explicitly declared dependencies. Added all
    missing coreutils, git, nix, ghq, and other dependencies to each
    script that requires them.

--- default.nix
+++ default.nix
@@ -3,58 +3,120 @@ let
   scripts = {
     kill-fzf = [
       pkgs.fzf
-      pkgs.ps
+      pkgs.procps
+      pkgs.gawk
     ];
-    get-repo = [ pkgs.gh ];
-    git-rebase-easy = [ pkgs.fzf ];
+    get-repo = [
+      pkgs.gh
+      pkgs.gnused
+      pkgs.coreutils
+      pkgs.ghq
+    ];
+    git-rebase-easy = [
+      pkgs.fzf
+      pkgs.git
+      pkgs.gnugrep
+      pkgs.gnused
+      pkgs.coreutils
+    ];
     clipboard = [
       pkgs.xclip
       pkgs.wl-clipboard
     ];
-    git-branch-fetch = [ pkgs.fzf ];
-    nix-store-repair = [ ];
-    nix-gcroot = [ ];
+    git-branch-fetch = [
+      pkgs.fzf
+      pkgs.git
+      pkgs.gnused
+      pkgs.findutils
+    ];
+    nix-store-repair = [
+      pkgs.nix
+    ];
+    nix-gcroot = [
+      pkgs.nix
+      pkgs.coreutils
+    ];
     tl = [ pkgs.translate-shell ];
-    list-desktops = [ ];
+    list-desktops = [
+      pkgs.coreutils
+      pkgs.findutils
+    ];
     kdec-share = [ pkgs.kdePackages.kdeconnect-kde ];
     cam = [
       pkgs.fzf
       pkgs.ffmpeg-full
+      pkgs.findutils
     ];
     get-my-repo = [
       pkgs.gh
       pkgs.fzf
+      pkgs.ghq
     ];
-    gc-full-nix = [ ];
-    git-root = [ ];
+    gc-full-nix = [
+      pkgs.nix
+      pkgs.doas
+      pkgs.coreutils
+    ];
+    git-root = [
+      pkgs.git
+    ];
     screenshot-each-monitor = [
       pkgs.maim
       pkgs.jc
       pkgs.jq
+      pkgs.xorg.xrandr
+      pkgs.coreutils
     ];
     screenshot-each-monitor-wayland = [
       pkgs.grim
       pkgs.jq
       pkgs.wlr-randr
+      pkgs.coreutils
     ];
-    open-match = [ pkgs.jq ];
-    ghq-gc = [ ];
+    open-match = [
+      pkgs.jq
+      pkgs.coreutils
+    ];
+    ghq-gc = [
+      pkgs.ghq
+      pkgs.git
+      pkgs.coreutils
+    ];
     ghq-nvim-nix = [
       pkgs.fzf
       pkgs.gawk
       pkgs.moreutils
       pkgs.findutils
+      pkgs.coreutils
+      pkgs.gnused
+      pkgs.git
+      pkgs.nix
+      pkgs.ghq
     ];
-    ghq-nix-flake-update = [ pkgs.jq ];
+    ghq-nix-flake-update = [
+      pkgs.jq
+      pkgs.gh
+      pkgs.ghq
+      pkgs.coreutils
+      pkgs.nix
+      pkgs.git
+      pkgs.gawk
+    ];
     dua-nix = [ pkgs.dua ];
-    npm-publish = [ pkgs.nodejs ];
+    npm-publish = [
+      pkgs.nodejs
+      pkgs.coreutils
+    ];
     run-gh-workflow = [
       pkgs.findutils
       pkgs.yq
       pkgs.fzf
+      pkgs.git
+      pkgs.coreutils
     ];
     hyprctl-cycle-mfact = [
       pkgs.jq
+      pkgs.hyprland
     ];
     chrome-by-email = [
       pkgs.jq
@@ -67,6 +129,7 @@ pkgs.lib.mapAttrs' (name: runtimeInputs: {
   value = pkgs.writeShellApplication {
     name = name;
     runtimeInputs = runtimeInputs;
+    inheritPath = false;
     text = builtins.readFile "${./scripts}/${name}.sh";
   };
 }) scripts
